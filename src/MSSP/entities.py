2dc7cfc6cfa15675163ab9746d7d094c53ace914
Brandon <brandon@starscream.nceas.ucsb.edu>
2015-10-28 16:20:09 -0700

    initial commit

---
+++ src/MSSP/entities.py
@@ -0,0 +1,29 @@
+'''
+Created on Oct 28, 2015
+
+@author: brandon
+
+Base entity class is just a set of attributes
+
+
+Four kinds of entities:
+
+ * Questions
+   - have user prompts
+   - have valid answer sets
+   
+ * Monitoring
+
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ src/MSSP/mssp_read.py
@@ -0,0 +1,64 @@
+'''
+Created on Oct 28, 2015
+
+@author: brandon
+This is a set of functions for reading grid and header content from the 
+MSSP spreadsheets, and formatting it into some semblance of a data
+structure.
+
+Overall strategy:
+the object types are defined based on the spreadsheet content each time
+(for later semantic management, perhaps?)
+
+
+Since there are three different MSSP spreadsheets, this class has at least
+three different functions to load the spreadsheets, however the processing
+is different for each spreadsheet:
+ * Monitoring
+   - questions by columns
+   - monitoring options by rows
+   - annotations in table grid
+   
+ * Assessment
+   - assessment methods by columns
+   - indices by rows (OPEN QUESTION: relation of these to Qs)
+   - minimum criteria in table grid
+   - some annotations also
+   
+ * Harvest Control Rules
+   - questions by rows
+   - rules by columns
+   - annotations in table grid
+
+From that come the entity types:
+ - monitoring options
+ - indices
+ - questions
+ - criteria
+    
+'''
+
+import openpyxl as xl
+
+grid_start = { "Monitoring":'F8',
+              "Assessment":'D37',
+              "HCR":'E9'}
+
+class MSSPSet(object):
+    '''
+    classdocs
+    '''
+    def read_monitoring(self, filepath, start=grid_start["Monitoring"]):
+        M = xl.load_workbook(filepath)
+        
+
+    def __init__(self, version='default'):
+        '''
+        Constructor.
+        version number is a text string to indicate the source of the files.
+        There should probably also be a directory specification- but that can
+        be contextual.
+        
+        '''
+        self.version = version
+        
