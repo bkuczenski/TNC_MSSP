3b99631113421bde6e02d60e58f3e51506ca7b50
Brandon Kuczenski <brandon.kuczenski@301south.net>
2016-04-29 13:24:25 -0700

    Rework write-to-JSON function

--- src/MSSP/__init__.py
+++ src/MSSP/__init__.py
@@ -6,4 +6,6 @@ from MSSP.importers.from_json import JsonImporter as MsspFromJson
 from MSSP.importers.from_spreadsheet import XlsImporter as MsspFromXls
 
 from MSSP.json_exch import write_json as write_to_json
+from MSSP.json_exch import read_json
+
 
